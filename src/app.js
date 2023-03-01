e27cf346966514c47c86814cb1b7e8d45e16ef7f
nghia.hoangchinh <nghia.hoangchinh@ascendcorp.com>
2023-03-01 22:29:46 +0700

    feat: lesson 3

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const compression = require('compression');
+const express = require('express');
+const { default: helmet } = require('helmet');
+const morgan = require('morgan');
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"))
+app.use(helmet());
+app.use(compression());
+
+
+//init db
+require('./dbs/init.mongodb')
+const {checkOverload} = require('./helpers/check.connect')
+checkOverload()
+
+// init routes
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello Fantipjs';
+    return res.status(200).json({
+        message: strCompress.repeat(10000)
+    })
+})
+
+// handle errror
+
+module.exports = app

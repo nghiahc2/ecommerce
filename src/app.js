f627ec269ab834e46dd6bb5686e5ff0b7cf72fd6
nghia.hoangchinh <nghia.hoangchinh@ascendcorp.com>
2023-03-30 15:55:24 +0700

    feat 8 : response class

--- src/app.js
+++ src/app.js
@@ -1,27 +1,43 @@
-require('dotenv').config()
-const compression = require('compression');
-const express = require('express');
-const { default: helmet } = require('helmet');
-const morgan = require('morgan');
+require("dotenv").config();
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
 const app = express();
 
 // init middlewares
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 //init db
-require('./dbs/init.mongodb')
+require("./dbs/init.mongodb");
 // const { checkOverload } = require('./helpers/check.connect')
 // checkOverload()
 
 // init routes
-app.use('/', require('./routes'))
+app.use("/", require("./routes"));
 
 // handle errror
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
 
-module.exports = app
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  return res.status(statusCode).json({
+    status: "error",
+    code: statusCode,
+    message: error.message || "Internal Error",
+  });
+});
+
+module.exports = app;
--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,15 +1,18 @@
 'use strict'
 
+const { CREATED } = require("../core/success.response")
 const AccessService = require("../services/access.service")
 
 class AccessController {
     signUp = async (req, res, next) => {
-        try {
-            console.log(`[P]::signUp::`, req.body)
-            return res.status(201).json(await AccessService.signUp(req.body))
-        } catch (error) {
-            next(error)
-        }
+        new CREATED({
+            message: 'Registerd!',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limits: 10
+            }
+        }).send(res)
+        //return res.status(201).json(await AccessService.signUp(req.body))
     }
 }
 
